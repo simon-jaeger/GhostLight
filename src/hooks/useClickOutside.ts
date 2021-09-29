0f9ac78d9a055278a678295c336bec52f70b1364
Simon Jäger <simon-jaeger@users.noreply.github.com>
2021-09-29 15:26:59 +0200

    ui: useClickOutside

---
+++ src/hooks/useClickOutside.ts
@@ -0,0 +1,12 @@
+import React, {useEffect} from "react"
+
+export function useClickOutside(ref: React.RefObject<any>, handler: () => void) {
+  function listener(e: MouseEvent) {
+    if (ref.current === null) return
+    if (!ref.current.contains(e.target as Node)) handler()
+  }
+  useEffect(() => {
+    window.addEventListener("click", listener)
+    return () => window.removeEventListener("click", listener)
+  }, [ref, handler])
+}
